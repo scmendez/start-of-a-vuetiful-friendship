662970410455d8bcc8cb1c72d60f18420d323ddb
Sandra Mendez <scmendez3b@gmail.com>
2020-11-25 14:42:37 +0100

    basic project setup from scratch

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import store from "./store";
+import { createApp } from 'vue';
+//createApp is a factory function that when called gives us a new Vue application
 
-createApp(App)
-  .use(store)
-  .mount("#app");
+const app = createApp();
+
+app.mount('#app')
+//need to mount it in the DOM, similar to React
+
+//to render something to the application, we need to create a root component

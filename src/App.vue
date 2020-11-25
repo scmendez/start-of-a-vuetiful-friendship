201e2cb07b9b1252652fe5d9a5fc93042cd3140c
Sandra Mendez <scmendez3b@gmail.com>
2020-11-25 15:47:20 +0100

    static message added in root vue component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,28 @@
-<template></template>
-<style></style>
-<script></script>
+<template>
+    <section>
+        <h1>{{ message }}</h1>
+        <h3>Hola mundo!</h3>
+    </section>
+</template>
+
+<!-- in order to access what's being returned within the script tags, must use {{  }} and the key name -->
+<!-- any property bound to the component itself can be directly accesed, no need for "this" -->
+
+<script>
+    export default {
+        data () {
+            return {
+                message: 'Hello World'
+            };
+        }
+    }
+</script>
+<!-- in object that you're exporting, you have a property "data" that is a function that returns an object that holds properties available elsewhere in the component and in the template itself -->
+<!-- only export ONE default object that has all of the definitions of the Vue js component (ex: single object can have data function, methods, computed properties, other components that can be used within the template, etc) -->
+
+<style lang="scss" >
+    @import './app.scss'
+</style>
+
+// OR  <style lang="scss" src="./app.scss"></style>
+// OR can write styles directly within the style tags
--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import { createApp } from 'vue';
 //createApp is a factory function that when called gives us a new Vue application
 
-const app = createApp();
+import App from './app.vue';
+//in order to render app.vue component as main component for application, need to 1) import App component from app.vue
+//Q: is app.vue already auto-exporting everything and that's the default, so could in theory name "App" anything since it's not named?
+
+const app = createApp(App);
+// 2) then pass this file to createApp function
+
 
 app.mount('#app')
 //need to mount it in the DOM, similar to React
+// 3) then we're mounting it to the DOM
+
 
 //to render something to the application, we need to create a root component
